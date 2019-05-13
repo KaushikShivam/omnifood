76b22515cbbc0765f860aa6d89550886a4e5b88b
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-05-13 18:43:36 +0530

    Add animated scroll

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -13,12 +13,16 @@ $(document).ready(function() {
 		}
 	);
 
-	//scroll on buttons
-	// js--scroll-to-plans
-	// js--scroll-to-start
 	$('.js--scroll-to-plans').click(function() {
 		$('html, body').animate(
 			{ scrollTop: $('.js--section-plans').offset().top },
+			1000
+		);
+	});
+
+	$('.js--scroll-to-start').click(function() {
+		$('html, body').animate(
+			{ scrollTop: $('.js--section-features').offset().top },
 			1000
 		);
 	});
